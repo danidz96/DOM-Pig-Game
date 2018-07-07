0a016d84ee62974f8d53762b16fc60c3fa970221
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-07-07 15:28:46 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*
+REGLAS DEL JUEGO:
+
+- El juego tiene 2 jugadores, jugando en rondas
+- En cada turno, un jugador tira un dado tantas veces como desee. Cada resultado se agrega a la cuenta de la ronda.
+- Pero, si el jugador saca un 1, se pierde toda su puntuación de la ronda. Después de eso, es el turno del siguiente jugador.
+- El jugador puede elegir 'Mantener', lo que significa que su puntuación de la ronda se agrega a su puntuación Global. Después de eso, es el turno del siguiente jugador.
+- El primer jugador en alcanzar los 100 puntos en el global gana el juego.
+*/
